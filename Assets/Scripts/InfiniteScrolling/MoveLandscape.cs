20b826f5626a47e9b3dab80392062617e174363a
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-08-18 13:12:54 -0300

    background updated to pixel landscape

---
+++ Assets/Scripts/InfiniteScrolling/MoveLandscape.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLandscape : MonoBehaviour
+{
+    public float speed = 1f;
+    public Transform target;
+
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+    }
+
+    void Update()
+    {
+        float targetEnd = target.position.x - 30;
+        float targetOrigin = target.position.x + 20;
+
+        if (transform.position.x < targetEnd)
+        {
+            transform.position = new Vector3(targetOrigin, transform.position.y, transform.position.z);
+        }
+    }
+}
